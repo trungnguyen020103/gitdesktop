ab1a016b1419d2cb0c4a4db2239130e17b8a111c
trungnguyen020103 <21130589@st.hcmuaf.edu.vn>
2025-03-28 18:44:37 +0700

    testUPlan1
    ssss

---
+++ BE/demoBE/src/main/java/com/example/demobe/DemoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBeApplication.class, args);
+    }
+
+}
---
+++ BE/demoBE/src/test/java/com/example/demobe/DemoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
